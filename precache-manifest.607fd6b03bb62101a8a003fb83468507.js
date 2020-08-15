4502658349d5f3e16e1963be04cf257488d74ca1
Marius Malaiska <mariusmalaiska@gmail.com>
2020-08-15 12:18:55 +0300

    Updates

---
+++ precache-manifest.607fd6b03bb62101a8a003fb83468507.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16e32e7bd91b8d59997bf3bb55dfd684",
+    "url": "/Vinted-copy-frontend/index.html"
+  },
+  {
+    "revision": "2b835aab1e71376be893",
+    "url": "/Vinted-copy-frontend/static/css/main.001cb8bd.chunk.css"
+  },
+  {
+    "revision": "2be193a61e34f57f9e32",
+    "url": "/Vinted-copy-frontend/static/js/2.5d0de5f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Vinted-copy-frontend/static/js/2.5d0de5f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b835aab1e71376be893",
+    "url": "/Vinted-copy-frontend/static/js/main.9de9fa5d.chunk.js"
+  },
+  {
+    "revision": "23018eb5b8e6fb19cbbd",
+    "url": "/Vinted-copy-frontend/static/js/runtime-main.8e926876.js"
+  },
+  {
+    "revision": "0cec0c5c2f064bbf8c0fb38f30c3777a",
+    "url": "/Vinted-copy-frontend/static/media/bell.0cec0c5c.png"
+  },
+  {
+    "revision": "fb4f923f667e2d612c5948cbdccfafb4",
+    "url": "/Vinted-copy-frontend/static/media/dress.fb4f923f.svg"
+  },
+  {
+    "revision": "7ba322ba7a58832279df0f87d8872fa1",
+    "url": "/Vinted-copy-frontend/static/media/email.7ba322ba.png"
+  },
+  {
+    "revision": "8c7b45fc169e1394eb7738441a8825e6",
+    "url": "/Vinted-copy-frontend/static/media/heart.8c7b45fc.png"
+  },
+  {
+    "revision": "d5e4bff3abb7a96ee3189ba1eb0822ca",
+    "url": "/Vinted-copy-frontend/static/media/home-hero.d5e4bff3.jpg"
+  },
+  {
+    "revision": "5d6fd07b5a283b4f6f1d2e0ede6aa91e",
+    "url": "/Vinted-copy-frontend/static/media/home-svg-mobile.5d6fd07b.svg"
+  },
+  {
+    "revision": "fe9282d282bb01e774060ced75af34e8",
+    "url": "/Vinted-copy-frontend/static/media/home-svg.fe9282d2.svg"
+  },
+  {
+    "revision": "3dcf8b0273b8b84dd6608d1fdc020851",
+    "url": "/Vinted-copy-frontend/static/media/logo.3dcf8b02.png"
+  },
+  {
+    "revision": "06d15745c861f4bfecbdd8b8cfa4de1e",
+    "url": "/Vinted-copy-frontend/static/media/shirt.06d15745.svg"
+  }
+]);
